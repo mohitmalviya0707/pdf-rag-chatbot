62954ae857cbd804a724479e71a2c1d0a37a98a1
Mohit Malviya <mohitmalviya0107@gmail.com>
2026-04-06 19:23:16 +0530

    Update app.py

--- rag_project/app.py
+++ rag_project/app.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_mistralai import ChatMistralAI
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate  
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 from langdetect import detect
